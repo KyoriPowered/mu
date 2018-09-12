e4e4b02fae07369ae2e28e85e7f597f5f0cb9331
kashike <kashike@vq.lc>
2018-09-11 19:49:18 -0700

    collections + iterables

---
+++ src/main/java/net/kyori/lambda/collection/MoreCollections.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of lambda, licensed under the MIT License.
+ *
+ * Copyright (c) 2018 KyoriPowered
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package net.kyori.lambda.collection;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.Collection;
+import java.util.function.Function;
+
+/*
+ * Name is prefixed with 'More' to avoid conflict with java.util.Collections
+ */
+
+/**
+ * A collection of utilities for working with collections.
+ */
+public interface MoreCollections {
+  /**
+   * Reduces a collection to a single element of the inhabiting type depending on the collection's size.
+   *
+   * <p>If the collection contains zero elements, {@code empty} will be chosen. A singleton collection will have its only
+   * element selected. Otherwise, {@code function} is used to reduce the collection to a single element of its type.</p>
+   *
+   * @param collection the collection
+   * @param empty the empty element
+   * @param function the flattener
+   * @param <E> the element type
+   * @return an element
+   */
+  static <E> /* @Nullable */ E squash(final @NonNull Collection<? extends E> collection, final /* @Nullable */ E empty, final @NonNull Function<Iterable<? extends E>, ? extends E> function) {
+    switch(collection.size()) {
+      case 0: return empty;
+      case 1: return collection.iterator().next();
+    }
+    return MoreIterables.reduce(collection, function);
+  }
+}
